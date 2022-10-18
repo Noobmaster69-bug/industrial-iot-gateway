47127d635c9fe0483cb5e4d19ea862291bdffccf
Noobmaster69-bug <tranuyson@gmail.com>
2022-10-18 10:19:20 +0700

    fix some bug

--- core/config_gen.js
+++ core/config_gen.js
@@ -1,28 +1,28 @@
 require("dotenv").config();
 const {
-  logging,
-  out_file,
-  error_file,
-  origin,
-  http_port,
-  https_port,
-  ssl_cert,
-  ssl_key,
-  ssl_ca,
+  LOGGING,
+  OUT_FILE,
+  ERROR_FILE,
+  ORIGIN,
+  HTTP_PORT,
+  HTTPS_PORT,
+  SSL_CERT,
+  SSL_KEY,
+  SSL_CA,
 } = process.env;
 
 const fs = require("fs");
 fs.writeFileSync(
   "./config.json",
   JSON.stringify({
-    logging,
-    out_file,
-    error_file,
-    origin,
-    http_port,
-    https_port,
-    cert: ssl_cert,
-    key: ssl_key,
-    ca: [ssl_ca],
+    logging: JSON.parse(LOGGING.toLowerCase()),
+    out_file: OUT_FILE,
+    error_file: ERROR_FILE,
+    origin: ORIGIN,
+    http_port: Number(HTTP_PORT),
+    https_port: Number(HTTPS_PORT),
+    cert: SSL_CERT,
+    key: SSL_KEY,
+    ca: [SSL_CA],
   })
 );
