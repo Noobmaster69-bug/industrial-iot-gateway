b80601b35b8646f8e9112fd850a1b50393419d89
miles <tranuyson@gmail.com>
2023-01-02 20:17:12 +0700

    update

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/miles/Workspace/industrial-iot-gateway/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/miles/Workspace/industrial-iot-gateway/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/miles/Workspace/industrial-iot-gateway/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/miles/Workspace/industrial-iot-gateway/docs/src/css/custom.css'),
-];
