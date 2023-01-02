f65adee619f42c25c2a5f5c7146bfaf468f3712c
miles <tranuyson@gmail.com>
2023-01-02 21:14:58 +0700

    update api

---
+++ docs/docs/development/development.md
@@ -0,0 +1,34 @@
+---
+sidebar_position: 1
+---
+
+# Development document
+
+## Repo workspace
+
+Recommend sử dụng [visual studio code](https://code.visualstudio.com/).
+
+Các extension được sử dụng:
+
+- [Sqlite3 viewer](https://marketplace.visualstudio.com/items?itemName=qwtel.sqlite-viewer): dùng để xem database.
+- [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode): auto format code.
+- [ESlint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint): code linting cho ReactJS.
+- [OpenAPI](https://marketplace.visualstudio.com/items?itemName=42Crunch.vscode-openapi): chỉnh sửa OpenAPI file.
+
+Các package được sử dụng:
+
+- [Widdershins](https://www.npmjs.com/package/widdershins): Dùng để generate markdown từ OpenAPI.
+- [Swagger UI](https://www.npmjs.com/package/open-swagger-ui): Serve một swagger ui server tại local để người dùng xem được API.
+- [Swagger Editor](https://www.npmjs.com/package/openapi-editor): Server một swagger editor server tại local để người dùng chỉnh sửa được API.
+
+### Core workspace
+
+Các package được sử dụng:
+
+- [gulp](https://www.npmjs.com/package/gulp): tool dùng để tự động hoá quá trình biên dịch, chạy thử code.
+- [rollup](https://www.npmjs.com/package/rollup): tool dùng để biên dịch và gói gọn code lại thành một file.
+- [@rollup/plugin-typescript](https://www.npmjs.com/package/@rollup/plugin-typescript): plugin dùng để biên dịch typescript.
+- [tslib](https://www.npmjs.com/package/tslib): Hỗ trợ [@rollup/plugin-typescript](https://www.npmjs.com/package/@rollup/plugin-typescript) biên dịch typescript.
+- [@rollup/plugin-json](https://www.npmjs.com/package/@rollup/plugin-json): plugin dùng để include file có định dạng `.json` vào code.
+- [gulp-nodemon](https://www.npmjs.com/package/gulp-nodemon): auto restart code khi code thay đổi
+- [pkg](https://www.npmjs.com/package/pkg): dùng để xuất ra binary file cho từng hệ thống khác nhau.
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
-    locales: ["en"],
+    locales: ["en", "vi"],
   },
 
   presets: [
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Industrial IoT Gateway",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -70,58 +70,61 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/Noobmaster69-bug/industrial-iot-gateway",
             label: "GitHub",
             position: "right",
+          },
+
+          {
+            type: "docsVersionDropdown",
           },
         ],
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        // links: [
+        //   {
+        //     title: "Docs",
+        //     items: [
+        //       {
+        //         label: "Tutorial",
+        //         to: "/docs/intro",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: "Community",
+        //     items: [
+        //       {
+        //         label: "Stack Overflow",
+        //         href: "https://stackoverflow.com/questions/tagged/docusaurus",
+        //       },
+        //       {
+        //         label: "Discord",
+        //         href: "https://discordapp.com/invite/docusaurus",
+        //       },
+        //       {
+        //         label: "Twitter",
+        //         href: "https://twitter.com/docusaurus",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: "More",
+        //     items: [
+        //       {
+        //         label: "Blog",
+        //         to: "/blog",
+        //       },
+        //       {
+        //         label: "GitHub",
+        //         href: "https://github.com/facebook/docusaurus",
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © 2023 Industrial IoT Gateway. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
